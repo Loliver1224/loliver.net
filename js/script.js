9dbdf5d6e8cf6737ce243613f4c2e54dc6d280ed
Loliver1224 <swim.01060808@gmail.com>
2020-05-24 19:58:06 +0900

    :sparkles: remake all

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.getElementById('trans-am').addEventListener('click', function () {
+  document.body.style.background = '#d63072';
+  document.getElementsByTagName('a')[0].style.color = 'white';
+  document.getElementsByTagName('a')[1].style.color = 'white';
+  document.getElementsByTagName('h1')[0].style.color = 'black';
+  document.getElementById('trans-am').style.display = 'none';
+});
